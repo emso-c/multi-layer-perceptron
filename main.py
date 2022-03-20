6e412466aeff30de5a788919583967378c490903
emso-c <32599085+emso-c@users.noreply.github.com>
2022-03-20 18:14:43 +0300

    Add bias & summation options to main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from modules.neural_network import MultiLayerPerceptron
 from modules.layer import Layer, generate_random_layer
 from modules.activation_functions import ActivationFunctions
+from modules.summation_functions import SummationFunctions
 from modules.utils import generate_random_input_data
 
 from modules.cli import single_neuron_creation_CLI
@@ -13,7 +14,9 @@ if __name__ == '__main__':
 
     INPUT_AMOUNT = 5
     LAYER_AMOUNT = 3
+    BIAS = 1
     ACTIVATION_FUNCTION = ActivationFunctions.TANH
+    SUMMATION_FUNCTION = SummationFunctions.WEIGHTED_TOTAL
     NORMALIZATION_SCALE = (-1,1)
     MIN_LAYER_DEPTH, MAX_LAYER_DEPTH = 1, 3
     MIN_INPUT_VALUE, MAX_INPUT_VALUE = -1000000, 1000000
@@ -28,7 +31,9 @@ if __name__ == '__main__':
     layers = [generate_random_layer(
         layer_depth=random.randint(MIN_LAYER_DEPTH, MAX_LAYER_DEPTH),
         activation_function=ACTIVATION_FUNCTION,
+        summation_function=SUMMATION_FUNCTION,
         normalization_scale=NORMALIZATION_SCALE,
+        bias=BIAS,
     ) for _ in range(LAYER_AMOUNT)]
 
     mlp = MultiLayerPerceptron(
