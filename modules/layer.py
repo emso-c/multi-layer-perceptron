1deedaa394c698fb841e5c91b07e977d4306098b
emso-c <32599085+emso-c@users.noreply.github.com>
2022-03-20 16:45:45 +0300

    Add bias & summation to Layer

--- modules/layer.py
+++ modules/layer.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 from .perceptron import Perceptron
 from .activation_functions import ActivationFunction, Sigmoid
+from .summation_functions import SummationFunction, WeightedTotal
 
 class Layer:
     def __init__(self, neurons:list[Perceptron]):
@@ -25,13 +26,17 @@ class Layer:
 def generate_random_layer(
         layer_depth:int,
         activation_function:ActivationFunction=Sigmoid,
+        summation_function:SummationFunction=WeightedTotal,
         normalization_scale:Tuple=(0,1),
+        bias:float=1,
     ) -> Layer:
 
     layer = Layer([])
     for _ in range(layer_depth):
         layer.add_neuron(Perceptron(
             activation_function=activation_function,
-            normalization_scale=normalization_scale
+            normalization_scale=normalization_scale,
+            summation_function=summation_function,
+            bias=bias,
         ))
     return layer
